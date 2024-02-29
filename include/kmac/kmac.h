8adad98f9739b2e7eab029ce5a9393550be7dbdc
Krispin Davies <krispin.davies@live.ca>
2024-02-28 21:22:28 -0500

    Established kmac package.

---
+++ include/kmac/kmac.h
@@ -0,0 +1,4 @@
+// Copyright (c) 2024 Krispin Davies
+// SPDX-License-Identifier: BSD-3-Clause
+
+#pragma once
---
+++ src/kmac.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2024 Krispin Davies
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include "kmac/kmac.h"
+
+#include <iostream>
+#include <tuple>
+
+int main(int argc, char **argv)
+{
+  std::ignore = argc;
+  std::ignore = argv;
+  std::cout << "Hello world." << std::endl;
+}
